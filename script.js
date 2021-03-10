7ac38040ba81f86005577e65854590ba664feb4b
psi <cy.lonsido@gmail.com>
2021-03-10 21:30:52 +0800

    play sound

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
+var url = "https://proxy.notificationsounds.com/message-tones/to-the-point-568/download/file-sounds-1111-to-the-point.mp3"
 function playSound() {
   var audio = new Audio(url);
-  // audio.play();
-  alert('asdf');
+  audio.play();
 }
 
+var btn = document.querySelector('#btn');
+btn.onclick = function() {
+  playSound();
+}
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function(event) {
   if (document.readyState == 'complete') {
-    console.log('ready');
+    btn.click();
   }
 });
