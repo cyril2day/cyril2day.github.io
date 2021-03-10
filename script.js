99cde2ab5ad52d7fc5f9e6b0bb1c4d2b6471707f
psi <cy.lonsido@gmail.com>
2021-03-10 21:19:20 +0800

    sample commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function playSound() {
+  var audio = new Audio(url);
+  // audio.play();
+  alert('asdf');
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  if (document.readyState == 'complete') {
+    console.log('ready');
+  }
+});
